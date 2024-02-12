a58a60500415d86dd5ddd31847e3fc61ea3e90b3
Luis Eduardo <loistronics@gmail.com>
2024-02-12 14:54:03 -0400

    Conexion a la API - Funcion Capital

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ByCapitalPageComponent {
+
+  public countries : Country[] = [];
+
+  constructor(private service : CountriesService){}
+
+  searchByCapital(term : string) : void{
+    this.service.searchCapital(term).subscribe(data => {
+      this.countries = data;
+      console.log(this.countries);
+    });
+  }
 
 }
