34c942de67eb6cb8844900eb4596a21d103c414d
Luis Eduardo <loistronics@gmail.com>
2024-02-18 10:36:33 -0400

    Funcionalidades Listas

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{  
+
+  public country?: Country;
+
+  constructor(private activateRoute : ActivatedRoute, private service : CountriesService, private router : Router){}
+
+  ngOnInit(): void {
+    this.activateRoute.params.pipe(switchMap(({id}) => this.service.searchCountryAlphaCode(id))).subscribe(data => {
+
+      if(!data){
+        return this.router.navigateByUrl('');
+      }
+      return this.country = data;
+    })
+  }
+  
 
 }
